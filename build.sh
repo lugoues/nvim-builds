6010f623af2b8a1600fef42fb7b2d43e88dd90be
Peter Brunner <peter@lugoues.net>
2024-05-02 23:42:11 -0400

    add build scripts

---
+++ build.sh
@@ -0,0 +1,21 @@
+mkdir -p $PWD/output
+cat <<-'EOF' | docker run -i --rm -v $PWD/output:/output debian bash -
+NVIM_VERSION=0.9.5
+
+apt-get update && apt-get -y install ninja-build gettext cmake unzip curl build-essential git file
+
+git clone --depth 1 --branch "v${NVIM_VERSION}"  https://github.com/neovim/neovim
+
+pushd neovim
+cmake -S cmake.deps -B .deps -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build .deps
+cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -D OUTPUT_NAME=nvim-${NVIM_VERSION}-$(uname -s | awk '{print tolower($0)}')-$(dpkg --print-architecture)
+cmake --build build
+
+pushd build
+cpack -G DEB -D CPACK_PACKAGE_DIRECTORY=/output -D CPACK_PACKAGE_FILE_NAME=nvim-${NVIM_VERSION}-$(uname -s | awk '{print tolower($0)}')-$(dpkg --print-architecture)
+cpack -G TGZ -D CPACK_PACKAGE_DIRECTORY=/output -D CPACK_PACKAGE_FILE_NAME=nvim-${NVIM_VERSION}-$(uname -s | awk '{print tolower($0)}')-$(dpkg --print-architecture)
+cpack -G TXZ -D CPACK_PACKAGE_DIRECTORY=/output -D CPACK_PACKAGE_FILE_NAME=nvim-${NVIM_VERSION}-$(uname -s | awk '{print tolower($0)}')-$(dpkg --print-architecture)
+cpack -G TBZ2 -D CPACK_PACKAGE_DIRECTORY=/output -D CPACK_PACKAGE_FILE_NAME=nvim-${NVIM_VERSION}-$(uname -s | awk '{print tolower($0)}')-$(dpkg --print-architecture)
+
+EOF
